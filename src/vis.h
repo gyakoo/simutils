abb1aacd2fcff453e49f13992bae3aa19750cc40
manu marin <gyakoo@gmail.com>
2015-09-09 21:02:14 -0400

    improved low-level gfx API in vis based on dx12.

--- src/vis.h
+++ src/vis.h
@@ -82,9 +82,13 @@ SOFTWARE.
 #endif
 
 //////////////////////////////////////////////////////////////////////////////
-//////////////////////////// RESOURCE TYPES //////////////////////////////////
+//////////////////////////// TYPES          //////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////
-#define VIS_NOFLAGS 0
+#if defined(VIS_DX11) || defined(VIS_DX12) || defined(VIS_VK) || defined(VIS_GL)
+#define VIS_MAX_RT 8
+#endif
+
+#define VIS_NONE 0
 #define VIS_LOAD_SOURCE_MEMORY 0
 #define VIS_LOAD_SOURCE_FILE 1
 
@@ -93,10 +97,218 @@ SOFTWARE.
 #define VIS_TYPE_SHADER 3
 #define VIS_TYPE_PIPELINE 4
 #define VIS_TYPE_INPUT_LAYOUT 5
-#define VIS_TYPE_GPU_LAYOUT 6
+#define VIS_TYPE_SHADER_LAYOUT 6
 #define VIS_TYPE_COMMAND_LIST 7
 #define VIS_TYPE_FENCE 8
 #define VIS_TYPE_STAGING_RES 9
+
+#define VIS_FORMAT_UNKNOWN 0
+#define VIS_FORMAT_R32G32B32A32_TYPELESS 1
+#define VIS_FORMAT_R32G32B32A32_FLOAT 2
+#define VIS_FORMAT_R32G32B32A32_UINT 3
+#define VIS_FORMAT_R32G32B32A32_SINT 4
+#define VIS_FORMAT_R32G32B32_TYPELESS 5
+#define VIS_FORMAT_R32G32B32_FLOAT 6
+#define VIS_FORMAT_R32G32B32_UINT 7
+#define VIS_FORMAT_R32G32B32_SINT 8
+#define VIS_FORMAT_R16G16B16A16_TYPELESS 9
+#define VIS_FORMAT_R16G16B16A16_FLOAT 10
+#define VIS_FORMAT_R16G16B16A16_UNORM 11
+#define VIS_FORMAT_R16G16B16A16_UINT 12
+#define VIS_FORMAT_R16G16B16A16_SNORM 13
+#define VIS_FORMAT_R16G16B16A16_SINT 14
+#define VIS_FORMAT_R32G32_TYPELESS 15
+#define VIS_FORMAT_R32G32_FLOAT 16
+#define VIS_FORMAT_R32G32_UINT 17
+#define VIS_FORMAT_R32G32_SINT 18
+#define VIS_FORMAT_R32G8X24_TYPELESS 19
+#define VIS_FORMAT_D32_FLOAT_S8X24_UINT 20
+#define VIS_FORMAT_R32_FLOAT_X8X24_TYPELESS 21
+#define VIS_FORMAT_X32_TYPELESS_G8X24_UINT 22
+#define VIS_FORMAT_R10G10B10A2_TYPELESS 23
+#define VIS_FORMAT_R10G10B10A2_UNORM 24
+#define VIS_FORMAT_R10G10B10A2_UINT 25
+#define VIS_FORMAT_R11G11B10_FLOAT 26
+#define VIS_FORMAT_R8G8B8A8_TYPELESS 27
+#define VIS_FORMAT_R8G8B8A8_UNORM 28
+#define VIS_FORMAT_R8G8B8A8_UNORM_SRGB 29
+#define VIS_FORMAT_R8G8B8A8_UINT 30
+#define VIS_FORMAT_R8G8B8A8_SNORM 31
+#define VIS_FORMAT_R8G8B8A8_SINT 32
+#define VIS_FORMAT_R16G16_TYPELESS 33
+#define VIS_FORMAT_R16G16_FLOAT 34
+#define VIS_FORMAT_R16G16_UNORM 35
+#define VIS_FORMAT_R16G16_UINT 36
+#define VIS_FORMAT_R16G16_SNORM 37
+#define VIS_FORMAT_R16G16_SINT 38
+#define VIS_FORMAT_R32_TYPELESS 39
+#define VIS_FORMAT_D32_FLOAT 40
+#define VIS_FORMAT_R32_FLOAT 41
+#define VIS_FORMAT_R32_UINT 42
+#define VIS_FORMAT_R32_SINT 43
+#define VIS_FORMAT_R24G8_TYPELESS 44
+#define VIS_FORMAT_D24_UNORM_S8_UINT 45
+#define VIS_FORMAT_R24_UNORM_X8_TYPELESS 46
+#define VIS_FORMAT_X24_TYPELESS_G8_UINT 47
+#define VIS_FORMAT_R8G8_TYPELESS 48
+#define VIS_FORMAT_R8G8_UNORM 49
+#define VIS_FORMAT_R8G8_UINT 50
+#define VIS_FORMAT_R8G8_SNORM 51
+#define VIS_FORMAT_R8G8_SINT 52
+#define VIS_FORMAT_R16_TYPELESS 53
+#define VIS_FORMAT_R16_FLOAT 54
+#define VIS_FORMAT_D16_UNORM 55
+#define VIS_FORMAT_R16_UNORM 56
+#define VIS_FORMAT_R16_UINT 57
+#define VIS_FORMAT_R16_SNORM 58
+#define VIS_FORMAT_R16_SINT 59
+#define VIS_FORMAT_R8_TYPELESS 60
+#define VIS_FORMAT_R8_UNORM 61
+#define VIS_FORMAT_R8_UINT 62
+#define VIS_FORMAT_R8_SNORM 63
+#define VIS_FORMAT_R8_SINT 64
+#define VIS_FORMAT_A8_UNORM 65
+#define VIS_FORMAT_R1_UNORM 66
+#define VIS_FORMAT_R9G9B9E5_SHAREDEXP 67
+#define VIS_FORMAT_R8G8_B8G8_UNORM 68
+#define VIS_FORMAT_G8R8_G8B8_UNORM 69
+#define VIS_FORMAT_BC1_TYPELESS 70
+#define VIS_FORMAT_BC1_UNORM 71
+#define VIS_FORMAT_BC1_UNORM_SRGB 72
+#define VIS_FORMAT_BC2_TYPELESS 73
+#define VIS_FORMAT_BC2_UNORM 74
+#define VIS_FORMAT_BC2_UNORM_SRGB 75
+#define VIS_FORMAT_BC3_TYPELESS 76
+#define VIS_FORMAT_BC3_UNORM 77
+#define VIS_FORMAT_BC3_UNORM_SRGB 78
+#define VIS_FORMAT_BC4_TYPELESS 79
+#define VIS_FORMAT_BC4_UNORM 80
+#define VIS_FORMAT_BC4_SNORM 81
+#define VIS_FORMAT_BC5_TYPELESS 82
+#define VIS_FORMAT_BC5_UNORM 83
+#define VIS_FORMAT_BC5_SNORM 84
+#define VIS_FORMAT_B5G6R5_UNORM 85
+#define VIS_FORMAT_B5G5R5A1_UNORM 86
+#define VIS_FORMAT_B8G8R8A8_UNORM 87
+#define VIS_FORMAT_B8G8R8X8_UNORM 88
+#define VIS_FORMAT_R10G10B10_XR_BIAS_A2_UNORM 89
+#define VIS_FORMAT_B8G8R8A8_TYPELESS 90
+#define VIS_FORMAT_B8G8R8A8_UNORM_SRGB 91
+#define VIS_FORMAT_B8G8R8X8_TYPELESS 92
+#define VIS_FORMAT_B8G8R8X8_UNORM_SRGB 93
+#define VIS_FORMAT_BC6H_TYPELESS 94
+#define VIS_FORMAT_BC6H_UF16 95
+#define VIS_FORMAT_BC6H_SF16 96
+#define VIS_FORMAT_BC7_TYPELESS 97
+#define VIS_FORMAT_BC7_UNORM 98
+#define VIS_FORMAT_BC7_UNORM_SRGB 99
+#define VIS_FORMAT_AYUV 100
+#define VIS_FORMAT_Y410 101
+#define VIS_FORMAT_Y416 102
+#define VIS_FORMAT_NV12 103
+#define VIS_FORMAT_P010 104
+#define VIS_FORMAT_P016 105
+#define VIS_FORMAT_420_OPAQUE 106
+#define VIS_FORMAT_YUY2 107
+#define VIS_FORMAT_Y210 108
+#define VIS_FORMAT_Y216 109
+#define VIS_FORMAT_NV11 110
+#define VIS_FORMAT_AI44 111
+#define VIS_FORMAT_IA44 112
+#define VIS_FORMAT_P8 113
+#define VIS_FORMAT_A8P8 114
+#define VIS_FORMAT_B4G4R4A4_UNORM 115
+#define VIS_FORMAT_P208 130
+#define VIS_FORMAT_V208 131
+#define VIS_FORMAT_V408 132
+
+#define VIS_BLEND_ZERO            1
+#define VIS_BLEND_ONE             2
+#define VIS_BLEND_SRC_COLOR       3
+#define VIS_BLEND_INV_SRC_COLOR   4
+#define VIS_BLEND_SRC_ALPHA       5
+#define VIS_BLEND_INV_SRC_ALPHA   6
+#define VIS_BLEND_DEST_ALPHA      7
+#define VIS_BLEND_INV_DEST_ALPHA  8
+#define VIS_BLEND_DEST_COLOR      9
+#define VIS_BLEND_INV_DEST_COLOR  10
+#define VIS_BLEND_SRC_ALPHA_SAT   11
+#define VIS_BLEND_BLEND_FACTOR    14
+#define VIS_BLEND_INV_BLEND_FACTOR 15
+#define VIS_BLEND_SRC1_COLOR       16
+#define VIS_BLEND_INV_SRC1_COLOR   17
+#define VIS_BLEND_SRC1_ALPHA       18
+#define VIS_BLEND_INV_SRC1_ALPHA   19
+
+#define VIS_BLEND_OP_ADD          1
+#define VIS_BLEND_OP_SUBTRACT     2
+#define VIS_BLEND_OP_REV_SUBTRACT 3
+#define VIS_BLEND_OP_MIN          4
+#define VIS_BLEND_OP_MAX          5
+
+#define VIS_LOGIC_OP_CLEAR            0
+#define VIS_LOGIC_OP_SET              1
+#define VIS_LOGIC_OP_COPY             2
+#define VIS_LOGIC_OP_COPY_INVERTED    3
+#define VIS_LOGIC_OP_NOOP             4
+#define VIS_LOGIC_OP_INVERT           5
+#define VIS_LOGIC_OP_AND              6
+#define VIS_LOGIC_OP_NAND             7
+#define VIS_LOGIC_OP_OR               8
+#define VIS_LOGIC_OP_NOR              9
+#define VIS_LOGIC_OP_XOR              10
+#define VIS_LOGIC_OP_EQUIV            11
+#define VIS_LOGIC_OP_AND_REVERSE      12
+#define VIS_LOGIC_OP_AND_INVERTED     13
+#define VIS_LOGIC_OP_OR_REVERSE       14
+#define VIS_LOGIC_OP_OR_INVERTED      15
+
+#define VIS_STAGE_IA (1<<0) // Input Assembler
+#define VIS_STAGE_VS (1<<1) // Vertex Shader
+#define VIS_STAGE_HS (1<<2) // Hull Shader
+#define VIS_STAGE_DS (1<<3) // Domain Shader
+#define VIS_STAGE_GS (1<<4) // Geometry Shader
+#define VIS_STAGE_PS (1<<5) // Pixel Shader
+#define VIS_STAGE_SO (1<<6) // Stream Out
+
+#define VIS_REG_TEXTURE (1<<0)      // SRV
+#define VIS_REG_MULTISHARED (1<<1)  // UAV
+#define VIS_REG_CBUFFER (1<<2)      // CBV
+#define VIS_REG_SAMPLER (1<<3)      // Sampler
+
+#define VIS_FILL_WIREFRAME 0
+#define VIS_FILL_SOLID 1
+
+#define VIS_CULL_NONE 0
+#define VIS_CULL_FRONT 1
+#define VIS_CULL_BACK 2
+
+#define VIS_PRIM_UNDEFINED  0
+#define VIS_PRIM_POINT      1
+#define VIS_PRIM_LINE       2
+#define VIS_PRIM_TRIANGLE   3
+#define VIS_PRIM_PATCH      4
+
+#define VIS_COMPFUNC_NEVER 1
+#define VIS_COMPFUNC_LESS 2
+#define VIS_COMPFUNC_EQUAL 3
+#define VIS_COMPFUNC_LESS_EQUAL 4
+#define VIS_COMPFUNC_GREATER 5
+#define VIS_COMPFUNC_NOT_EQUAL 6
+#define VIS_COMPFUNC_GREATER_EQUAL 7
+#define VIS_COMPFUNC_ALWAYS 8
+
+#define VIS_STENCIL_OP_KEEP 1
+#define VIS_STENCIL_OP_ZERO 2
+#define VIS_STENCIL_OP_REPLACE 3
+#define VIS_STENCIL_OP_INCR_SAT 4
+#define VIS_STENCIL_OP_DECR_SAT 5
+#define VIS_STENCIL_OP_INVERT 6
+#define VIS_STENCIL_OP_INCR 7
+#define VIS_STENCIL_OP_DECR 8
+
+#define VIS_MSAA_QUALITY_STANDARD 0xffffffff
+#define VIS_MSAA_QUALITY_CENTER 0xfffffffe
 
 #ifdef __cplusplus
 extern "C" {
@@ -128,13 +340,7 @@ extern "C" {
     vis3x3  view;
     vis3    pos;
   }vis_camera;
-
-  ////////////////////////////////////////////////
-  typedef struct vis_pipeline_desc
-  {
-
-  }vis_pipeline_desc;
-
+  
   ////////////////////////////////////////////////
   typedef struct vis_id
   {
@@ -143,7 +349,131 @@ extern "C" {
     uint32_t value;
   };
 
-  typedef struct vis_sbc;
+  ////////////////////////////////////////////////
+  typedef struct vis_input_element
+  {
+    const char* semantic;
+    uint32_t sem_index;
+    uint32_t format;
+    uint32_t slot;   
+    uint32_t offset;
+  }vis_input_element;
+
+  ////////////////////////////////////////////////
+  typedef struct vis_shader_registers
+  {
+    uint32_t reg_type;    // VIS_REG*
+    uint32_t base_reg;    
+    uint32_t reg_count;
+    uint32_t visibility;  // VIS_STAGE* flags
+  }vis_shader_registers;
+
+  typedef struct vis_shader_layout
+  {
+    vis_shader_registers* reg_desc;
+    uint32_t reg_desc_count;
+    uint32_t stage_flags;             // VIS_STAGE* flags
+  };
+
+  ////////////////////////////////////////////////
+  typedef struct vis_blend_target
+  {
+    uint8_t enabled;            // bool
+    uint8_t blend_src;          // VIS_BLEND*
+    uint8_t blend_dst;          // VIS_BLEND*
+    uint8_t blend_op;           // VIS_BLEND_OP*
+    uint8_t blendalpha_src;     // VIS_BLEND*
+    uint8_t blendalpha_dst;     // VIS_BLEND*
+    uint8_t blendalpha_op;      // VIS_BLEND_OP*
+    uint8_t logic_op_enabled;   // bool
+    uint8_t logic_op;           // VIS_LOGIC_OP
+    uint8_t rt_writemask;       // 0-0xff
+  }vis_blend_target;
+
+  typedef struct vis_blend_state
+  {
+    uint8_t alpha_to_coverage;                // bool
+    uint8_t independent_blend;                // bool  
+    vis_blend_target  blend_rt[VIS_MAX_RT];
+  }vis_blend_state;
+
+  ////////////////////////////////////////////////
+  typedef struct vis_raster_state
+  {
+    uint8_t fill_mode;            // VIS_FILL*
+    uint8_t cull_mode;            // VIS_CULL*
+    uint8_t is_front_ccw;         // bool
+    int32_t depth_bias;           //
+    float depth_bias_clamp;       //
+    float slope_scaled_depth_bias;//
+    uint8_t depth_clip_enabled;   // bool
+    uint8_t msaa_enabled;         // bool
+    uint8_t aaline_enabled;       // bool
+    uint8_t force_sample_count;   // 0-12
+    uint8_t conservative_enabled; // bool
+  }vis_raster_state;
+
+  ////////////////////////////////////////////////
+  typedef struct vis_stencil_op
+  {
+    uint8_t op_fail;        // VIS_STENCIL_OP_*
+    uint8_t op_depth_fail;  // VIS_STENCIL_OP_*
+    uint8_t op_pass_op;     // VIS_STENCIL_OP_*
+    uint8_t compfunc;       // VIS_COMPFUNC_*
+  }vis_stencil_op;
+
+  typedef struct vis_depth_stencil_state
+  {
+    uint8_t depth_enabled;    // bool
+    uint8_t depth_write_all;  // bool
+    uint8_t depth_compfunc;   // VIS_COMPFUNC_*
+
+    uint8_t stencil_enabled;    // bool
+    uint8_t stencil_read_mask;  // 0-0xff
+    uint8_t stencil_write_mask; // 0-0xff
+    vis_stencil_op op_front;
+    vis_stencil_op op_back;
+  }vis_depth_stencil_state;
+
+  ////////////////////////////////////////////////
+  typedef struct vis_stream_out_state
+  {
+    uint8_t enabled;
+    uint32_t unused;
+  }vis_stream_out_state;
+
+  ////////////////////////////////////////////////
+  typedef struct vis_pipeline_state
+  {
+    vis_resource shader_layout;    
+    vis_resource vs;
+    vis_resource ps;
+    vis_resource ds;
+    vis_resource hs;
+    vis_resource gs;
+    vis_input_element* vertex_layout;
+    uint32_t vertex_layout_count;
+    vis_stream_out_state stream_out_state;
+    vis_blend_state blend_state;
+    vis_raster_state raster_state;
+    vis_depth_stencil_state depth_stencil_state;
+    uint8_t depth_stencil_format;                 // VIS_FORMAT_*
+    uint32_t blend_sample_mask;
+    uint8_t primitive_topology;                   // VIS_PRIM*
+    uint32_t rt_count;                            
+    uint8_t rt_formats[VIS_MAX_RT];               // VIS_FORMAT_*      
+    uint8_t msaa_count;                           // multisamples per pixel
+    uint32_t msaa_quality;                        // can be also VIS_MSAA_QUALITY_*
+    uint32_t gpu_node;                            // 0 for single-gpu.
+    uint8_t debug_enabled;                        // bool
+  }vis_pipeline_state;
+
+  ////////////////////////////////////////////////
+  typedef struct vis_shader_bytecode
+  {
+    uint8_t* data;
+    uint32_t size;
+  }vis_sbc;
 
   typedef vis_id vis_resource;  
   typedef vis_id vis_staging;
@@ -159,27 +489,23 @@ extern "C" {
   vis_resource vis_create_resource(vis* vi, uint16_t type, void* resData, uint32_t flags);  
 
   ////////////////////////////////////////////////
-  uint32_t vis_shader_compile(vis* vi, uint32_t loadSrc, void* srcData, uint32_t srcSize, vis_sbc** outByteCode );
-  void vis_release_sbc(vis* vi, vis_sbc** byteCode);
+  uint32_t vis_shader_compile(vis* vi, uint32_t loadSrc, void* srcData, uint32_t srcSize, vis_shader_bytecode* outByteCode );
+  void vis_pipeline_state_set_default(vis* vi, vis_pipeline_state* pstate);
 
   ////////////////////////////////////////////////
-  uint32_t vis_command_list_record(vis* vi, vis_command_list cl);
-  vis_staging vis_command_list_resource_update(vis* vi, vis_command_list cl, vis_resource res, void* inData, uint32_t inSize);
+  uint32_t vis_command_list_record(vis* vi, vis_resource cmdList);
+  vis_staging vis_command_list_resource_update(vis* vi, vis_resource cmdList, vis_resource res, void* inData, uint32_t inSize);
   uint32_t vis_command_list_release_update(vis* vi, vis_staging stag);
-  uint32_t vis_command_list_close(vis* vi, vis_command_list cl);
-  uint32_t vis_command_list_execute(vis* vi, vis_command_list* lists, uint32_t count);
+  uint32_t vis_command_list_close(vis* vi, vis_resource cmdList);
+  uint32_t vis_command_list_execute(vis* vi, vis_resource* cmdLists, uint32_t count);
 
   ////////////////////////////////////////////////
-  void vis_sync_gpu_to_signal();
-  void vis_sync_cpu_wait_for_signal();
-  void vis_sync_cpu_callback_when_signal();
+  void vis_sync_gpu_to_signal(vis* vi);
+  void vis_sync_cpu_wait_for_signal(vis* vi);
+  void vis_sync_cpu_callback_when_signal(vis* vi);
 
   ////////////////////////////////////////////////
-  void vis_release_descriptor();
-  void vis_release_resource();
-  void vis_release_pipeline();
-  void vis_release_command_list();
-
+  void vis_release_resource(vis* vi, vis_resource resource);
 
   /* ************************************************************************************************ */
   /* ************************************************************************************************ */
--- utils/dx12test/dx12test.cc
+++ utils/dx12test/dx12test.cc
@@ -23,7 +23,6 @@ struct app_assets
   vis_resource vb;
   vis_resource vs;
   vis_resource ps;
-  vis_resource vertex_layout;
   vis_resource shader_layout;
 };
 
@@ -35,7 +34,7 @@ struct app_vertex
 
 int app_load_assets(vis* vi, app_assets* assets)
 {
-  // Define the geometry for a triangle.
+  // Define the geometry for a triangle, the vertex and pixel shaders for it
   app_vertex triangleVertices[] =
   {
     { { 0.0f, 0.25f * vi->aspect_ratio, 0.0f },{ 1.0f, 0.0f, 0.0f, 1.0f } },
@@ -43,59 +42,76 @@ int app_load_assets(vis* vi, app_assets* assets)
     { { -0.25f, -0.25f * vi->aspect_ratio, 0.0f },{ 0.0f, 0.0f, 1.0f, 1.0f } }
   };
 
+  vis_input_element vinput[] = 
+  {
+    { "POSITION", 0, VIS_FORMAT_R32G32B32_FLOAT, 0, 0 },
+    { "COLOR", 0, VIS_FORMAT_R32G32B32_FLOAT, 0, 12 },
+  };
   const char* vsstr = "";
   const char* psstr = "";
 
   // Resources (data) - not filling data in yet
-  assets->vb = vis_create_resource(vi, VIS_TYPE_VERTEXBUFFER, nullptr, VIS_NOFLAGS);
-  vis_sbc* sbc=nullptr;
-  vis_shader_compile(vi, VIS_LOAD_SOURCE_MEMORY, (void*)vsstr, strlen(vsstr), &sbc);
-  assets->vs = vis_create_resource(vi, VIS_TYPE_SHADER, sbc, VIS_NOFLAGS);
-  vis_release_sbc(vi, &sbc);
-  vis_shader_compile(vi, VIS_LOAD_SOURCE_MEMORY, (void*)psstr, strlen(psstr), &sbc);
-  assets->ps = vis_create_resource(vi, VIS_TYPE_SHADER, sbc, VIS_NOFLAGS);
-  vis_release_sbc(vi, &sbc);
+  {
+    assets->vb = vis_create_resource(vi, VIS_TYPE_VERTEXBUFFER, nullptr, VIS_NONE);
+    
+    vis_shader_bytecode sbc = { 0 };
+    vis_shader_compile(vi, VIS_LOAD_SOURCE_MEMORY, (void*)vsstr, strlen(vsstr), &sbc);
+    assets->vs = vis_create_resource(vi, VIS_TYPE_SHADER, &sbc, VIS_NONE);
+
+    vis_shader_compile(vi, VIS_LOAD_SOURCE_MEMORY, (void*)psstr, strlen(psstr), &sbc);
+    assets->ps = vis_create_resource(vi, VIS_TYPE_SHADER, &sbc, VIS_NONE);
+  }
 
   // Descriptors (metadata)
-  assets->vertex_layout = vis_create_resource(vi, VIS_TYPE_INPUT_LAYOUT, inputLayout, VIS_NOFLAGS);
-  assets->shader_layout = vis_create_resource(vi, VIS_TYPE_GPU_LAYOUT, shLayout, VIS_NOFLAGS);
+  {
+    vis_shader_layout shaderLayout = {
+      nullptr,  // no shader registers
+      0,
+      VIS_STAGE_IA | VIS_STAGE_VS | VIS_STAGE_PS
+    };
+    assets->shader_layout = vis_create_resource(vi, VIS_TYPE_SHADER_LAYOUT, &shaderLayout, VIS_NONE);
+  }
 
   // Pipeline state
-  vis_pipeline_desc pdesc = { 0 };
-  pdesc.vertex_layout = assets->vertex_layout;
-  pdesc.shader_layout = assets->shader_layout;
-  pdesc.vs = assets->vs;
-  pdesc.ps = assets->ps;
-  assets->pipeline = vis_create_pipeline(vi, &pdesc);
+  {
+    vis_pipeline_state pstate;
+    vis_pipeline_state_set_default(vi, &pstate);
+    pstate.vertex_layout = vinput;
+    pstate.vertex_layout_count = 2;
+    pstate.shader_layout = assets->shader_layout;
+    pstate.vs = assets->vs;
+    pstate.ps = assets->ps;
+    assets->pipeline = vis_create_resource(vi, VIS_TYPE_PIPELINE, &pstate, VIS_NONE);
+  }
 
-  // Command list
-  assets->cmd_list = vis_create_command_list(vi, assets->pipeline);  
-  // Record some upload to GPU in the cmd list
-  vis_command_list_record(vi, assets->cmd_list);
-  vis_staging vbupdate = vis_command_list_resource_update(vi, assets->cmd_list, assets->vb, triangleVertices, sizeof(app_vertex));
-  vis_command_list_close(vi, assets->cmd_list );
-  vis_command_list_execute(vi, &assets->cmd_list, 1);
+  // Command list. creates and record a VB upload data, then waits for it
+  {
+    assets->cmd_list = vis_create_resource(vi, VIS_TYPE_COMMAND_LIST, &assets->pipeline, VIS_NONE);
+    // Record some upload to GPU in the cmd list
+    vis_command_list_record(vi, assets->cmd_list);
+    vis_staging vbupdate = vis_command_list_resource_update(vi, assets->cmd_list, assets->vb, triangleVertices, sizeof(app_vertex));
+    vis_command_list_close(vi, assets->cmd_list);
+    vis_command_list_execute(vi, &assets->cmd_list, 1);
 
-  // tell gpu to signal once it finishes and wait for it here
-  vis_id signal = vis_sync_gpu_to_signal(vi);
-  vis_sync_cpu_wait_for_signal(vi, signal); // vis_sync_cpu_callback_when_signal(vi, signal, cb_func, cb_data);
+    // tell gpu to signal once it finishes and wait for it here
+    vis_id signal = vis_sync_gpu_to_signal(vi);
+    vis_sync_cpu_wait_for_signal(vi, signal); // vis_sync_cpu_callback_when_signal(vi, signal, cb_func, cb_data);
 
-  // release temporary resources allocated for upload
-  vis_command_list_release_update(vi, assets->cmd_list, vbupdate);
+    // release temporary resources allocated for upload
+    vis_command_list_release_update(vi, assets->cmd_list, vbupdate);
+  }
 
   return VIS_OK;
 }
 
 void app_unload_assets(vis* vi, app_assets* assets)
 {
-  vis_release_descriptor(vi, assets->vertex_layout);
-  vis_release_descriptor(vi, assets->gpu_layout);
+  vis_release_resource(vi, assets->shader_layout);
   vis_release_resource(vi, assets->ps);
   vis_release_resource(vi, assets->vs);
   vis_release_resource(vi, assets->vb);
-
-  vis_release_pipeline(vi, assets->pipeline);
-  vis_release_command_list(vi, assets->cmd_list);
+  vis_release_resource(vi, assets->pipeline);
+  vis_release_resource(vi, assets->cmd_list);
 }
 
 int main(int argc, const char** argv)
